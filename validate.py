0ca9d9b0a93bfd54838a9a2d0ebb5fafd2bc811a
Ankit Bindal <ankitb.1997@gmail.com>
2018-05-01 23:07:52 +0530

    Changed indentation to spaces

--- validate.py
+++ validate.py
@@ -5,28 +5,28 @@
 import tensorflow as tf
 
 def validation(sess, 
-	model, 
-	dataLoader,
-	writer,
-	epoch):
+    model, 
+    dataLoader,
+    writer,
+    epoch):
 
-	total_loss = 0.0
+    total_loss = 0.0
 
-	for sensor, label in dataLoader.next_validation():
-		# Run the graph.
-		loss, tb = sess.run([model['loss'], 
-							model['summary']], 
-							feed_dict={
-								model['sensor_data']: sensor,
-								model['label']: label
-							})
-		total_loss += loss
+    for sensor, label in dataLoader.next_validation():
+        # Run the graph.
+        loss, tb = sess.run([model['loss'], 
+                            model['summary']], 
+                            feed_dict={
+                                model['sensor_data']: sensor,
+                                model['label']: label
+                            })
+        total_loss += loss
 
-	avg_loss = total_loss / dataLoader.valid_batches
-	print("======================================")
-	print("Validation loss : {}".format(avg_loss))
-	print("======================================")
+    avg_loss = total_loss / dataLoader.valid_batches
+    print("======================================")
+    print("Validation loss : {}".format(avg_loss))
+    print("======================================")
 
-	writer.add_summary(tb, global_step=epoch)
+    writer.add_summary(tb, global_step=epoch)
 
-	return avg_loss
+    return avg_loss
